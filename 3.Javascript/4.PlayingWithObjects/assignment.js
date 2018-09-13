9181f886dc01d4fd9275c96468227c681ac300b1
algorithmite <algorithmite@gmail.com>
2018-09-13 14:06:34 -0500

    Object Iteration Code

---
+++ 3.Javascript/4.PlayingWithObjects/assignment.js
@@ -0,0 +1,11 @@
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+for (idx in users) {
+	if (users[idx].name == "John") {
+		console.log(users[idx].age);
+		break;
+	}
+}
+console.log(users[0].name);
+for (idx in users) {
+	console.log(users[idx].name + " - " + users[idx].age);
+}
