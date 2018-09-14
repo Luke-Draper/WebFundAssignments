a2e1622d53e84e9345fe3d1b57836b559a74c704
Luke Draper <lukepdraper@gmail.com>
2018-09-13 22:39:59 -0500

    Jquery 101

--- 4.jQuery/1.jQueryFunctions/functions.js
+++ 4.jQuery/1.jQueryFunctions/functions.js
@@ -1,9 +1,61 @@
 $(document).ready(function(){
 
-    $('#addClass button').click(function(){
-        $('#addClass p').addClass('red');
-    });
-
-    // put your code for the remaining functions below
+	$('#click button').click(function(){
+		console.log("Click");
+	});
+	$('#hide button').click(function(){
+		$('#hide span').hide();
+	});
+	$('#show button').click(function(){
+		$('#hide span').show();
+	});
+	$('#toggle button').click(function(){
+		$('#toggle span').toggle();
+	});
+	$('#slideDown button').click(function(){
+		$('#slideDown p').hide();
+		$('#slideDown p').slideDown();
+	});
+	$('#slideUp button').click(function(){
+		$('#slideUp p').slideUp(400, function () {
+			$(this).show()
+		});
+	});
+	$('#slideToggle button').click(function(){
+		$('#slideToggle p').slideToggle();
+	});
+	$('#fadeOut button').click(function(){
+		$('#fadeOut p').fadeOut(400, function () {
+			$(this).show()
+		});
+	});
+	$('#fadeIn button').click(function(){
+		$('#fadeIn p').hide();
+		$('#fadeIn p').fadeIn();
+	});
+	$('#addClass button').click(function(){
+		$('#addClass p').addClass('red');
+	});
+	$('#before button').click(function(){
+		$('#before p').before("<span>Before </span>");
+	});
+	$('#after button').click(function(){
+		$('#after p').after("<span>After </span>");
+	});
+	$('#append button').click(function(){
+		$('#append p').append(" Append.");
+	});
+	$('#html button').click(function(){
+		$('#html p').html("<strong>HTML</strong>");
+	});
+	$('#attr button').click(function(){
+		console.log($('#attr p').attr('id'));
+	});
+	$('#val button').click(function(){
+		console.log($('#val textarea').val());
+	});
+	$('#text button').click(function(){
+		$('#text p').text("Text");
+	});
 
 });
