76117bfe2fb4196142e8c264150bcab033037eb5
Luke Draper <lukepdraper@gmail.com>
2018-09-13 23:44:59 -0500

    Toggling Images

---
+++ 4.jQuery/3.NinjaToCat/functions.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('img').click(function(){
+		var imgCurrent = $(this).attr("src");
+		var altCurrent = $(this).attr("alt");
+		var imgNext = $(this).attr("other-src");
+		var altNext = $(this).attr("other-alt");
+		$(this).attr("src",imgNext);
+		$(this).attr("alt",altNext);
+		$(this).attr("other-src",imgCurrent);
+		$(this).attr("other-alt",altCurrent);
+	});
+});
