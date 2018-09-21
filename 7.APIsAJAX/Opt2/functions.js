84c712fe530f5bd8cd011a4955a62284c3284fbc
Luke Draper <lukepdraper@gmail.com>
2018-09-21 13:56:03 -0500

    Start Pokedex

--- 7.APIsAJAX/Opt2/functions.js
+++ 7.APIsAJAX/Opt2/functions.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 // "<img src='http://pokeapi.co/media/sprites/pokemon/"+i+".png'>"
 	var outhtml = "";
 	for (var i=1; i<=151; ++i) {
 		outhtml += "<img src='http://pokeapi.co/media/sprites/pokemon/"+i+".png'>"
 	}
 	$("#img-container").html(outhtml);
+});*/
+var pokemans = [];
+$(document).ready(function(){
+	$.get("https://pokeapi.co/api/v2/pokemon/12/", function(res) {
+		pokemans.push(res);
+		console.log(pokemans);
+	}, "json");
+	$.get("https://pokeapi.co/api/v2/pokemon/11/", function(res) {
+		pokemans.push(res);
+		console.log(pokemans);
+	}, "json");
 });
